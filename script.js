68e52ab05397d813525aa5125af3f7bd2ba6582a
Marco-2009 <pariz.marco@escola.pr.gov.br>
2025-10-21 10:15:43 -0300

    21/10

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ botaoMostraPalavras.addEventListener("click",mostraPalavrasChave);
 
 function mostraPalavrasChave(){
 const texto = document.querySelector("#entrada-de-texto").value;
-const campoResultado = document.querySelector("resultado-palavrachave");
+const campoResultado = document.querySelector("#resultado-palavrachave");
 const palavras = texto.split(" ");
 
 campoResultado.textContent = palavras.join(", ")
